bba94302dceebe5940f0bd8aea5adc4a6131d559
Marya Kaysina <linkinpark335@gmail.com>
2023-05-18 17:31:57 +0300

    fix import json

--- server/firebase.js
+++ server/firebase.js
@@ -1,6 +1,9 @@
+import { createRequire } from "module";
 import { initializeApp, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 // import serviceAccount from './creds.json' assert { type: 'json' };
+
+const require = createRequire(import.meta.url);
 const serviceAccount = require('./creds.json');
 
 initializeApp({
